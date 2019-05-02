6dd9f6cc5c153d4ecdc048795cd6018578c142aa
Phil <PhilboBaggins@users.noreply.github.com>
2019-05-03 01:34:30 +1000

    Add an example

---
+++ examples/days-are-days-that-end-in-y.rs
@@ -0,0 +1,29 @@
+use is_an_enum::*;
+
+#[allow(dead_code)]
+#[derive(IsAnEnum)]
+#[derive(PartialEq, Debug)]
+enum Day {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+}
+
+impl Day {
+    fn does_it_end_in_y(&self) -> bool {
+        let s = format!("{:?}", self);
+        s.ends_with('y') || s.ends_with('Y')
+    }
+}
+
+fn main() {
+    if Day::Sunday.is_a(Day::Sunday) && Day::Sunday.does_it_end_in_y() {
+        println!("Sunday is a Sunday and it ends in \"y\"");
+    } else {
+        println!("The world is ending");
+    }
+}
