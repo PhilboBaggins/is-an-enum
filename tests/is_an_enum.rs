a05cc301ea6329bcad7fccce8e205615b506d851
Phil <PhilboBaggins@users.noreply.github.com>
2019-05-03 01:26:31 +1000

    More tests

--- tests/is_an_enum.rs
+++ tests/is_an_enum.rs
@@ -14,7 +14,7 @@ enum Day {
 }
 
 #[test]
-fn test_is_an_enum() {
+fn is_it_a_sunday() {
     assert!(true  == Day::Sunday.is_a_sunday());
     assert!(false == Day::Sunday.is_a_monday());
     assert!(false == Day::Sunday.is_a_tuesday());
@@ -22,4 +22,26 @@ fn test_is_an_enum() {
     assert!(false == Day::Sunday.is_a_thursday());
     assert!(false == Day::Sunday.is_a_friday());
     assert!(false == Day::Sunday.is_a_saturday());
+}
+
+#[test]
+fn is_it_a_monday() {
+    assert!(false == Day::Monday.is_a_sunday());
+    assert!(true  == Day::Monday.is_a_monday());
+    assert!(false == Day::Monday.is_a_tuesday());
+    assert!(false == Day::Monday.is_a_wednesday());
+    assert!(false == Day::Monday.is_a_thursday());
+    assert!(false == Day::Monday.is_a_friday());
+    assert!(false == Day::Monday.is_a_saturday());
+}
+
+#[test]
+fn is_it_a_tuesday() {
+    assert!(false == Day::Tuesday.is_a_sunday());
+    assert!(false == Day::Tuesday.is_a_monday());
+    assert!(true  == Day::Tuesday.is_a_tuesday());
+    assert!(false == Day::Tuesday.is_a_wednesday());
+    assert!(false == Day::Tuesday.is_a_thursday());
+    assert!(false == Day::Tuesday.is_a_friday());
+    assert!(false == Day::Tuesday.is_a_saturday());
 }
